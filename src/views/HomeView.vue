11a3f2caf2bbfb7a79883e4a47fdedf2fe6213ee
Richard Prandt <rprandt.contato@gmail.com>
2023-08-19 18:16:37 -0300

    build: add vuetify and tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
+<template>
+  <v-btn>Ola mudno</v-btn>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
